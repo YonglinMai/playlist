ea5cc13d7104ccdd9414b71a2bd5737b3e315a24
YonglinMai <yonglinmai@gmail.com>
2020-02-12 23:12:20 +0000

    02/12/2020

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var songName = [];
+var songArtist = [];
+var songImage = [];
+var songLength = [];
+var songLink = [];
+
+function displaySongInfo(){
+
+}
+function empty(){
+  $("#songs").empty();
+  $("#images").empty();
+  $("#artists").empty();
+  $("#lengths").empty();
+  $("#links").empty();  
+}
+
