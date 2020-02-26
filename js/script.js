3f2917124d576eccf644cfca355a799f2ef78a1a
YonglinMai <yonglinmai@gmail.com>
2020-02-26 23:23:12 +0000

    2-26-2020

--- js/script.js
+++ js/script.js
@@ -4,9 +4,25 @@ var songImage = [];
 var songLength = [];
 var songLink = [];
 
-function displaySongInfo(){
-
+function displaySongInfo(item1,item2,item3){
+    let item = $(item2).val();
+    item1.push(item);    
+    console.log(item1);
+    console.log("item:", item);
+    item1.forEach(function(newItem){
+        $(item3).append("<div>" + newItem + "</div>")
+    })
 }
+
+$("#add").click(function(){
+    empty();
+    displaySongInfo(songName,"#song","#songs");
+    displaySongInfo(songArtist,"#artist","#artists");
+    displaySongInfo(songImage,"#image","#images");
+    displaySongInfo(songLength,"#length","#lengths");
+    displaySongInfo(songLink,"#link","#links");
+
+})
 function empty(){
   $("#songs").empty();
   $("#images").empty();
